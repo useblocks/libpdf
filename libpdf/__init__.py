afc54e1307920f2c9d6c0ee22e0de399c05865c0
Marco Heinemann <marco.heinemann@useblocks.com>
2021-01-04 15:55:45 +0100

    black: fixed error

--- libpdf/__init__.py
+++ libpdf/__init__.py
@@ -17,9 +17,9 @@ except ImportError:
 __version__: str = importlib_metadata.version('libpdf')
 __summary__: str = importlib_metadata.metadata('libpdf')['Summary']
 
-import libpdf._import_forks  # noqa: E402, F401
-# below imports cannot be at the top avoid circular import errors in core.py when
+# below imports from libpdf.core cannot be at the top avoid circular import errors in core.py when
 # importing __version__ and __summary__
+import libpdf._import_forks  # noqa: E402, F401
 from libpdf.core import main_api as load  # noqa: E402
 from libpdf.core import main_cli  # noqa: E402
 
