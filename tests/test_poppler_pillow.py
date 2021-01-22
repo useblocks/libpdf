2dc388c4f51381a854d11b8c888aefca84a5ac13
Marco Heinemann <marco.heinemann@useblocks.com>
2021-01-22 10:56:40 +0100

    Fixed black issue

--- tests/test_poppler_pillow.py
+++ tests/test_poppler_pillow.py
@@ -4,4 +4,5 @@
 def test_run_poppler_pillow_example():
     """Import executes the code directly."""
     from examples import poppler_pillow  # pylint: disable=import-outside-toplevel
+
     del poppler_pillow
