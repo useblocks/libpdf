685807dde72ef731387e0a129d660c4f98aa6622
Marco Heinemann <marco.heinemann@useblocks.com>
2021-12-20 16:56:50 +0100

    Removed Win exception

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -18,11 +18,5 @@ def test_cli_ok(path):
     """Check if CLI exits with code 0 when no errors occur."""
     runner = CliRunner()
     result = runner.invoke(main_cli, [path, '-o', 'out.yaml', '-f', 'yaml'])
-    if sys.platform.startswith('win') and path == PDF_TWO_COLUMNS:
-        # TODO bug on Windows currently for PDF_TWO_COLUMNS:
-        #      UnicodeEncodeError('charmap', '\uf0b7  8 1/2', 0, 1, 'character maps to <undefined>')
-        assert isinstance(result.exception, UnicodeEncodeError)
-        assert result.exit_code == 1
-    else:
-        assert result.exception is None
-        assert result.exit_code == 0
+    assert result.exception is None
+    assert result.exit_code == 0
